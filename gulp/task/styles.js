83016ca0bc5aff81b582d4dcdba031cfd1af12db
FaisalAli19 <faisalali1901@gmail.com>
2016-12-09 14:51:08 +0530

    Fixed the mobile view issue

--- gulp/task/styles.js
+++ gulp/task/styles.js
@@ -5,11 +5,12 @@ cssvars = require("postcss-simple-vars"),
 nested = require("postcss-nested"),
 cssimport = require("postcss-import"),
 mixins = require("postcss-mixins"),
-hexrgba = require("postcss-hexrgba");
+hexrgba = require("postcss-hexrgba"),
+lost = require("lost");
 
 gulp.task("styles", function() {
     return gulp.src("./app/assets/styles/styles.css")
-        .pipe(postcss([cssimport, mixins, cssvars, nested, hexrgba, autoprefixer]))
+        .pipe(postcss([cssimport, mixins, cssvars, nested, hexrgba, lost, autoprefixer]))
         .on("error", function(info) {
             console.log(info.toString);
             this.emit("end");
