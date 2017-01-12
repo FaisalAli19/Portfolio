56c81b506078b477d1abef74dc6b8f20d9a7baea
FaisalAli19 <faisalali1901@gmail.com>
2017-01-12 11:56:51 +0530

    Completed form validation

--- app/assets/scripts/modules/validation.js
+++ app/assets/scripts/modules/validation.js
@@ -3,14 +3,13 @@ import $ from "jquery";
 class FormValidation {
     constructor() {
         this.contactForm = $("#contact-form");
-        this.inputName = $("#Name");
-        this.inputEmail = $("#Email");
-        this.inputMessage = $("#Message");
-        this.inputNameVal = $("#Name").val();
-        this.inputEmailVal = $("#Email").val();
-        this.inputMessageVal = $("#Message").val();
+        this.nameInput = $("form #Name");
+        this.emailInput = $("form #Email");
+        this.messageInput = $("form #Message");
         this.submitButton = $("form #submit");
-        this.nameRegEx = /^[a-zA-Z]+$/;
+        this.successMessage = $("form .form__messages--success");
+        this.errorMessage = $("form .form__messages--error");
+        this.nameRegEx = /^[a-zA-Z]+[ ]?[a-zA-Z]+$/;
         this.emailRegEx = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
         this.events();
     }
@@ -18,80 +17,119 @@ class FormValidation {
     events(){
         this.submitButton.click(this.validate.bind(this));
     }
-    //Validate all the fields by run the individual function
+
     validate(){
-        this.validateName();
-        this.validateEmail();
-        this.ValidateMessage();
-    }
-    //Check Name Input Field
-    validateName(){
-        //Check the length of the Name input
-        if(this.inputNameVal.length > 0){
-            //Check for valid name
-            if(this.inputNameVal == this.nameRegEx){
-                this.inputName.addClass("form__input--valid");
-                return false;
-            }
-            //if name has invalid character or number then add invalid class to input
-            else{
-                this.inputName.addClass("form__input--invalid");
-                return false;
-            }
-
+        this.nameValid();
+        this.emailValid();
+        this.messageValid();
+        //Check if all are Validated
+        if(this.nameValid() && this.emailValid() && this.messageValid()){
+            //if validated display success message and send messageValid
+            this.errorMessage.addClass("form__messages--hide");
+            this.successMessage.removeClass("form__messages--hide");
+            return false;
         }
-        //If input is empty add required class to Name input
-        else{
-            this.inputName.addClass("form__input--required");
+        else {
+            this.successMessage.addClass("form__messages--hide");
+            this.errorMessage.removeClass("form__messages--hide");
             return false;
         }
     }
-
-    //Check Email Input Field
-    validateEmail(){
-
-        //Check the length of the Email input
-        if(this.inputEmailVal.length > 0){
-            //Check for valid Email
-            if(this.inputEmailVal == this.emailRegEx){
-                this.inputEmail.addClass("form__input--valid");
-                return false;
-            }
-            //if Email is invalid then add invalid class to input
-            else{
-                this.inputEmail.addClass("form__input--invalid");
-                return false;
-            }
-
-        }
-        //If input is empty add required class to Email input
-        else{
-            this.inputEmail.addClass("form__input--required");
+    //Validate name field
+    nameValid(){
+        var nameLength = this.nameInput.val().length;
+        var nameVal = this.nameInput.val();
+        //check if length is zero
+        if(nameLength === 0){
+            //if length is zero add required class and notify user
+            this.nameInput.removeClass("form__input--valid");
+            this.nameInput.removeClass("form__input--invalid");
+            this.nameInput.addClass("form__input--required");
             return false;
+        }
+        //If length is not zero will check for valid input
+        else {
+            //This will match the input for regEx pattern
+            if(nameVal.match(this.nameRegEx)){
+                //If regEx match valid class will be added
+                this.nameInput.removeClass("form__input--required");
+                this.nameInput.removeClass("form__input--invalid");
+                this.nameInput.addClass("form__input--valid");
+                return true;
+            }
+            //If input doesn't match regEx will add invalid class and notify user
+            else {
+                //if regEx doesn't match invalid class will be added
+                this.nameInput.removeClass("form__input--required");
+                this.nameInput.removeClass("form__input--valid");
+                this.nameInput.addClass("form__input--invalid");
+                return false;
+            }
         }
     }
 
-    //Check Message Filed
-    ValidateMessage(){
-        //Check for minimum length
-        if(this.inputMessageVal.length > 0){
-
-            //if length is greater than 10 then add valid class
-            if(this.inputMessageVal.length > 10){
-                this.inputMessage.addClass("form__textarea--valid");
-                return false;
-            }
-            //if length is less than 10 then add invalid class
-            else{
-                this.inputMessage.addClass("form__textarea--invalid");
-                return false;
-            }
-
-        }
-        //if the message is below minimum length display required state
-        else{
-            this.inputMessage.addClass("form__textarea--required");
+    //Validate email field
+    emailValid(){
+        var emailLength = this.emailInput.val().length;
+        var emailVal = this.emailInput.val();
+        //check if length is zero
+        if(emailLength === 0){
+            //if length is zero add required class and notify user
+            this.emailInput.removeClass("form__input--valid");
+            this.emailInput.removeClass("form__input--invalid");
+            this.emailInput.addClass("form__input--required");
             return false;
+        }
+        //If length is not zero will check for valid input
+        else {
+            //This will match the input for regEx pattern
+            if(emailVal.match(this.emailRegEx)){
+                //If regEx match valid class will be added
+                this.emailInput.removeClass("form__input--required");
+                this.emailInput.removeClass("form__input--invalid");
+                this.emailInput.addClass("form__input--valid");
+                return true;
+            }
+            //If input doesn't match regEx will add invalid class and notify user
+            else {
+                //if regEx doesn't match invalid class will be added
+                this.emailInput.removeClass("form__input--required");
+                this.emailInput.removeClass("form__input--valid");
+                this.emailInput.addClass("form__input--invalid");
+                return false;
+            }
+        }
+    }
+
+    //Validate message field
+    messageValid(){
+        var messageLength = this.messageInput.val().length;
+        //check if length is zero
+        if(messageLength === 0){
+            //if length is zero add required class and notify user
+            this.messageInput.removeClass("form__textarea--valid");
+            this.messageInput.removeClass("form__textarea--invalid");
+            this.messageInput.addClass("form__textarea--required");
+            return false;
+        }
+        //If length is not zero will check for valid input
+        else {
+            //This will match the input for minimum length
+            if(messageLength > 10){
+                //If length match valid class will be added
+                this.messageInput.removeClass("form__textarea--required");
+                this.messageInput.removeClass("form__textarea--invalid");
+                this.messageInput.addClass("form__textarea--valid");
+                return true;
+            }
+            //If input doesn't match minimum length then will add invalid class and notify user
+            else {
+                //if length doesn't match invalid class will be added
+                this.messageInput.removeClass("form__textarea--required");
+                this.messageInput.removeClass("form__textarea--valid");
+                this.messageInput.addClass("form__textarea--invalid");
+                return false;
+            }
         }
     }
 }
--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -10357,14 +10357,13 @@
 	        _classCallCheck(this, FormValidation);
 
 	        this.contactForm = (0, _jquery2.default)("#contact-form");
-	        this.inputName = (0, _jquery2.default)("#Name");
-	        this.inputEmail = (0, _jquery2.default)("#Email");
-	        this.inputMessage = (0, _jquery2.default)("#Message");
-	        this.inputNameVal = (0, _jquery2.default)("#Name").val();
-	        this.inputEmailVal = (0, _jquery2.default)("#Email").val();
-	        this.inputMessageVal = (0, _jquery2.default)("#Message").val();
+	        this.nameInput = (0, _jquery2.default)("form #Name");
+	        this.emailInput = (0, _jquery2.default)("form #Email");
+	        this.messageInput = (0, _jquery2.default)("form #Message");
 	        this.submitButton = (0, _jquery2.default)("form #submit");
-	        this.nameRegEx = /^[a-zA-Z]+$/;
+	        this.successMessage = (0, _jquery2.default)("form .form__messages--success");
+	        this.errorMessage = (0, _jquery2.default)("form .form__messages--error");
+	        this.nameRegEx = /^[a-zA-Z]+[ ]?[a-zA-Z]+$/;
 	        this.emailRegEx = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
 	        this.events();
 	    }
@@ -10374,89 +10373,128 @@
 	        value: function events() {
 	            this.submitButton.click(this.validate.bind(this));
 	        }
-	        //Validate all the fields by run the individual function
-
 	    }, {
 	        key: "validate",
 	        value: function validate() {
-	            this.validateName();
-	            this.validateEmail();
-	            this.ValidateMessage();
+	            this.nameValid();
+	            this.emailValid();
+	            this.messageValid();
+	            //Check if all are Validated
+	            if (this.nameValid() && this.emailValid() && this.messageValid()) {
+	                //if validated display success message and send messageValid
+	                this.errorMessage.addClass("form__messages--hide");
+	                this.successMessage.removeClass("form__messages--hide");
+	                return false;
+	            } else {
+	                this.successMessage.addClass("form__messages--hide");
+	                this.errorMessage.removeClass("form__messages--hide");
+	                return false;
+	            }
 	        }
-	        //Check Name Input Field
+	        //Validate name field
 
 	    }, {
-	        key: "validateName",
-	        value: function validateName() {
-	            //Check the length of the Name input
-	            if (this.inputNameVal.length > 0) {
-	                //Check for valid name
-	                if (this.inputNameVal == this.nameRegEx) {
-	                    this.inputName.addClass("form__input--valid");
-	                    return false;
-	                }
-	                //if name has invalid character or number then add invalid class to input
-	                else {
-	                        this.inputName.addClass("form__input--invalid");
-	                        return false;
-	                    }
+	        key: "nameValid",
+	        value: function nameValid() {
+	            var nameLength = this.nameInput.val().length;
+	            var nameVal = this.nameInput.val();
+	            //check if length is zero
+	            if (nameLength === 0) {
+	                //if length is zero add required class and notify user
+	                this.nameInput.removeClass("form__input--valid");
+	                this.nameInput.removeClass("form__input--invalid");
+	                this.nameInput.addClass("form__input--required");
+	                return false;
 	            }
-	            //If input is empty add required class to Name input
+	            //If length is not zero will check for valid input
 	            else {
-	                    this.inputName.addClass("form__input--required");
-	                    return false;
-	                }
-	        }
-
-	        //Check Email Input Field
-
-	    }, {
-	        key: "validateEmail",
-	        value: function validateEmail() {
-
-	            //Check the length of the Email input
-	            if (this.inputEmailVal.length > 0) {
-	                //Check for valid Email
-	                if (this.inputEmailVal == this.emailRegEx) {
-	                    this.inputEmail.addClass("form__input--valid");
-	                    return false;
-	                }
-	                //if Email is invalid then add invalid class to input
-	                else {
-	                        this.inputEmail.addClass("form__input--invalid");
-	                        return false;
+	                    //This will match the input for regEx pattern
+	                    if (nameVal.match(this.nameRegEx)) {
+	                        //If regEx match valid class will be added
+	                        this.nameInput.removeClass("form__input--required");
+	                        this.nameInput.removeClass("form__input--invalid");
+	                        this.nameInput.addClass("form__input--valid");
+	                        return true;
 	                    }
-	            }
-	            //If input is empty add required class to Email input
-	            else {
-	                    this.inputEmail.addClass("form__input--required");
-	                    return false;
+	                    //If input doesn't match regEx will add invalid class and notify user
+	                    else {
+	                            //if regEx doesn't match invalid class will be added
+	                            this.nameInput.removeClass("form__input--required");
+	                            this.nameInput.removeClass("form__input--valid");
+	                            this.nameInput.addClass("form__input--invalid");
+	                            return false;
+	                        }
 	                }
 	        }
 
-	        //Check Message Filed
+	        //Validate email field
 
 	    }, {
-	        key: "ValidateMessage",
-	        value: function ValidateMessage() {
-	            //Check for minimum length
-	            if (this.inputMessageVal.length > 0) {
-
-	                //if length is greater than 10 then add valid class
-	                if (this.inputMessageVal.length > 10) {
-	                    this.inputMessage.addClass("form__textarea--valid");
-	                    return false;
-	                }
-	                //if length is less than 10 then add invalid class
-	                else {
-	                        this.inputMessage.addClass("form__textarea--invalid");
-	                        return false;
-	                    }
+	        key: "emailValid",
+	        value: function emailValid() {
+	            var emailLength = this.emailInput.val().length;
+	            var emailVal = this.emailInput.val();
+	            //check if length is zero
+	            if (emailLength === 0) {
+	                //if length is zero add required class and notify user
+	                this.emailInput.removeClass("form__input--valid");
+	                this.emailInput.removeClass("form__input--invalid");
+	                this.emailInput.addClass("form__input--required");
+	                return false;
 	            }
-	            //if the message is below minimum length display required state
+	            //If length is not zero will check for valid input
 	            else {
-	                    this.inputMessage.addClass("form__textarea--required");
-	                    return false;
+	                    //This will match the input for regEx pattern
+	                    if (emailVal.match(this.emailRegEx)) {
+	                        //If regEx match valid class will be added
+	                        this.emailInput.removeClass("form__input--required");
+	                        this.emailInput.removeClass("form__input--invalid");
+	                        this.emailInput.addClass("form__input--valid");
+	                        return true;
+	                    }
+	                    //If input doesn't match regEx will add invalid class and notify user
+	                    else {
+	                            //if regEx doesn't match invalid class will be added
+	                            this.emailInput.removeClass("form__input--required");
+	                            this.emailInput.removeClass("form__input--valid");
+	                            this.emailInput.addClass("form__input--invalid");
+	                            return false;
+	                        }
+	                }
+	        }
+
+	        //Validate message field
+
+	    }, {
+	        key: "messageValid",
+	        value: function messageValid() {
+	            var messageLength = this.messageInput.val().length;
+	            //check if length is zero
+	            if (messageLength === 0) {
+	                //if length is zero add required class and notify user
+	                this.messageInput.removeClass("form__textarea--valid");
+	                this.messageInput.removeClass("form__textarea--invalid");
+	                this.messageInput.addClass("form__textarea--required");
+	                return false;
+	            }
+	            //If length is not zero will check for valid input
+	            else {
+	                    //This will match the input for minimum length
+	                    if (messageLength > 10) {
+	                        //If length match valid class will be added
+	                        this.messageInput.removeClass("form__textarea--required");
+	                        this.messageInput.removeClass("form__textarea--invalid");
+	                        this.messageInput.addClass("form__textarea--valid");
+	                        return true;
+	                    }
+	                    //If input doesn't match minimum length then will add invalid class and notify user
+	                    else {
+	                            //if length doesn't match invalid class will be added
+	                            this.messageInput.removeClass("form__textarea--required");
+	                            this.messageInput.removeClass("form__textarea--valid");
+	                            this.messageInput.addClass("form__textarea--invalid");
+	                            return false;
+	                        }
 	                }
 	        }
 	    }]);
